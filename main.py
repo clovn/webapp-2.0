416521a276ee7c735b2c848eb978f426e4fc6453
Clovn <Haliullin101@mail.ru>
2023-02-23 18:48:11 +0300

    awda

--- main.py
+++ main.py
@@ -6,16 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/submit_form', methods=['POST'])
-def submit_form():
-    num = request.form.get('num')
-    date = request.form.get('date')
-    reason = request.form.get('reason')
-
-    # здесь можно добавить код для сохранения данных в базу данных или отправки их на email
-    # ...
-
-    return render_template('submit.html', num=num, date=date, reason=reason)
 
 if __name__ == '__main__':
     app.run(debug=True)
